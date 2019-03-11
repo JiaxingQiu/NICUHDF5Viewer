f1a539e888e69ed9a522bc8564224ca56f9f7aa2
Amanda Edwards <edwards.amandam@gmail.com>
2019-03-11 15:36:43 -0400

    Start incorporating Doug's code 2. Added in the Timestamp Menu callback to switch between localtime and time to event. Removed unneeded blocks of code.

--- HDF5Viewer_v1_0_1.m
+++ HDF5Viewer_v1_0_1.m
@@ -41,7 +41,7 @@ function varargout = HDF5Viewer_v1_0_1(varargin)
 
 % Edit the above text to modify the response to help HDF5Viewer_v1_0_1
 
-% Last Modified by GUIDE v2.5 11-Mar-2019 09:48:49
+% Last Modified by GUIDE v2.5 11-Mar-2019 14:08:06
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -108,26 +108,26 @@ function varargout = HDF5Viewer_v1_0_1_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-function edit1_Callback(hObject, eventdata, handles) % This keeps the background of the GUI from showing a figure
-% hObject    handle to edit1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit1 as text
-%        str2double(get(hObject,'String')) returns contents of edit1 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit1_CreateFcn(hObject, eventdata, handles)  % This keeps the background of the GUI from showing a figure
-% hObject    handle to edit1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
+% function edit1_Callback(hObject, eventdata, handles) % This keeps the background of the GUI from showing a figure
+% % hObject    handle to edit1 (see GCBO)
+% % eventdata  reserved - to be defined in a future version of MATLAB
+% % handles    structure with handles and user data (see GUIDATA)
+% 
+% % Hints: get(hObject,'String') returns contents of edit1 as text
+% %        str2double(get(hObject,'String')) returns contents of edit1 as a double
+% 
+% 
+% % --- Executes during object creation, after setting all properties.
+% function edit1_CreateFcn(hObject, eventdata, handles)  % This keeps the background of the GUI from showing a figure
+% % hObject    handle to edit1 (see GCBO)
+% % eventdata  reserved - to be defined in a future version of MATLAB
+% % handles    empty - handles not created until after all CreateFcns called
+% 
+% % Hint: edit controls usually have a white background on Windows.
+% %       See ISPC and COMPUTER.
+% if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+%     set(hObject,'BackgroundColor','white');
+% end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% MAIN CODE %%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -305,6 +305,7 @@ plotdata(hObject, eventdata, handles, overwrite);
 
 function plotdata(hObject, eventdata, handles,overwrite)
 % value = number(s) of the signal(s) chosen in Available Signals listbox
+
 if isfield(handles,'value')
     numsigs = length(handles.value);
     set(handles.tagalgstextbox,'string','')
@@ -325,6 +326,11 @@ else
     handles.numplots = numsigs;
 end
 
+% Deal with the Timestamp Dropdown Menu
+if ~isfield(handles,'tstampchoice')
+    handles.tstampchoice = 1;
+end
+
 handles.h = zeros(handles.numplots,1);
 
 % Loop through all of the selected items in Available Signals listbox
@@ -333,13 +339,16 @@ for s=1:numsigs
     handles.dataindex = find(ismember(handles.alldatasetnames,varname));
     if handles.dataindex<=length(handles.info.name) % Determine if we are plotting a results file - here we are not plotting a results file
         [data,~,~]=getfiledata(handles.info,varname);
-        [handles.vdata,handles.vname,handles.vt]=formatdata(data,handles.info,2,0);
+        if handles.tstampchoice==1 % Time to Event
+            [handles.vdata,handles.vname,handles.vt]=formatdata(data,handles.info,1,0);
+        elseif handles.tstampchoice==2 % Local Date
+            [handles.vdata,handles.vname,handles.vt]=formatdata(data,handles.info,3,0);
+        end
         handles.vdataindex = find(ismember(handles.vname,varname));
     else
         handles.rdataindex = find(ismember(handles.rname(:,1),varname));
     end
     
-    [plotcolor] = customplotcolors(varname);
     
     if handles.overlayon
         handles.h(s) = subplot(handles.numplots,1,1,'Parent',handles.PlotPanel);
@@ -361,7 +370,10 @@ for s=1:numsigs
         end
     end
     
-    handles.isutc = strcmp(handles.info.timezone,'UTC');
+    [plotcolor] = customplotcolors(varname);
+    
+%     handles.isutc = strcmp(handles.info.timezone,'UTC');
+    handles.isutc = 0;
     
     if handles.ishdf5 && handles.isutc % UTC time
         handles.windowsize = handles.windowsizeuserinput*60*1000; % 20 min in milliseconds is default
@@ -369,6 +381,7 @@ for s=1:numsigs
         handles.windowsize = handles.windowsizeuserinput*60/86400; 
     end
     
+    % Pull the data and timestamps we want to plot
     if handles.dataindex<=length(handles.info.name) % Vital Signs/Waveforms
         if ~isfield(handles,'startindex')
             handles.startindex = 1;
@@ -400,19 +413,31 @@ for s=1:numsigs
         handles.utctime = handles.rt(handles.startindexr:handles.endindexr);
     end
     handles.sig(handles.sig==-32768) = nan;
+    
+    % UTC Date vs Days Since Time 0
     if handles.isutc
         handles.localtime = utc2localwrapper(handles.utctime/1000,handles.info.timezone);
         set(handles.DayTextBox,'string','');
     else % the time is in matlab time
-        handles.localtime = handles.utctime; % WUSTL data is in datenum format already
-        if handles.windowstarttime<0
-            daytodisp = -day(-handles.windowstarttime);
-        else
-            daytodisp = day(handles.windowstarttime);
+        handles.localtime = handles.utctime; % Data is in datenum (days) format already
+        if handles.tstampchoice==1
+            if handles.windowstarttime<0
+                daytodisp = -day(-handles.windowstarttime);
+            else
+                daytodisp = day(handles.windowstarttime);
+            end
+            set(handles.DayTextBox,'string',['Day: ' num2str(daytodisp)]);
+        elseif handles.tstampchoice==2
+            formatOut = 'mm/dd/yy';
+            daytodisp = datestr(handles.windowstarttime,formatOut);
+            set(handles.DayTextBox,'string',daytodisp);
         end
-        set(handles.DayTextBox,'string',['Day: ' num2str(daytodisp)]);
+        
     end
+    
     I = ~isnan(handles.sig); % Don't try to plot the NaN values
+    
+    % Actually plot the data
     if isempty(I) || sum(I)==0
         handles.plothandle(s) = plot(0,0);
         handles.plothandleI(s).I = I;
@@ -422,11 +447,15 @@ for s=1:numsigs
         handles.plothandleI(s).I = I;
         ylabel(strrep(varname,'_',' '));
     end
+    
+    % Handle the ability to zoom in and out while maintaining reasonable timestamps
     if ~isdeployed
         addpath('zoomAdaptiveDateTicks');
     end
     zoomAdaptiveDateTicks('on')
     datetick('x',13)
+    
+    % Set the x-axis limits
     if handles.isutc
         xlim([utc2localwrapper(handles.windowstarttime/1000,handles.info.timezone) utc2localwrapper(handles.windowendtime/1000,handles.info.timezone)])
     else
@@ -493,37 +522,6 @@ end
 
 if contains(varname,'Results')
     ylim([0 1])
-end
-
-
-function data = scaledata(hObject, eventdata, handles, namesofinterest, data)
-% hObject    handle to RemoveTag (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-scalematrix = zeros(length(namesofinterest),1);
-for n=1:length(namesofinterest)
-    try
-        scalematrix(n) = double(h5readatt(fullfile(handles.pathname, handles.filename),[namesofinterest{n} '/data'],'Scale'));
-    catch
-        scalematrix(n) = 0;
-    end
-    % For layout version 3, scale is simply a multiplicative factor for the original value. To get the real value, you divide by scale. For layout version 4.0, a real value of 1.2 is stored as 12 with a scale of 1, where scale is stored as the power of 10, so to convert from 12 back to 1.2, you need to divide by 10^scale. Later in this code, when we are actually plotting the data, we divide by scale
-    try
-        handles.layoutversion = h5readatt(fullfile(handles.pathname, handles.filename),'/','Layout Version');
-    catch
-        handles.layoutversion = "Doug's Layout";
-    end
-    if ischar(handles.layoutversion)
-        majorversion = str2num(handles.layoutversion(1));
-        if majorversion~=3
-            scalematrix(n) = 10^scalematrix(n);
-        end
-    else
-        if str2double(handles.layoutversion{1}(1)) ~= 3 
-            scalematrix(n) = 10^scalematrix(n);
-        end
-    end
-    data(:,n) = data(:,n)/scalematrix(n);
 end
 
 
@@ -1163,3 +1161,37 @@ msgbox({
 'This program allows you to browse the contents of HDF5 files and run algorithms on the data';
 '';
 },'Help')
+
+
+% --- Executes on selection change in TimestampMenu.
+function TimestampMenu_Callback(hObject, eventdata, handles)
+% hObject    handle to TimestampMenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns TimestampMenu contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from TimestampMenu
+contents = cellstr(get(hObject,'String'));
+tstampchoice = contents{get(hObject,'Value')};
+if strcmp(tstampchoice,'Local Date')
+    handles.tstampchoice = 2;
+elseif strcmp(tstampchoice,'Time to Event')
+    handles.tstampchoice = 1;
+end
+handles = rmfield(handles,'windowstarttime');
+handles = rmfield(handles,'windowendtime');
+overwrite = 1;
+plotdata(hObject, eventdata, handles, overwrite);
+
+
+% --- Executes during object creation, after setting all properties.
+function TimestampMenu_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to TimestampMenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
